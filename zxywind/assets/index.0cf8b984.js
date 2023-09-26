2f7e2541a3566184d10e9b1a7c21546154f85cfa
likang <1825289506@qq.com>
2023-09-26 18:34:44 +0800

    fix

--- zxywind/assets/index.0cf8b984.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as _,c}from"./index.4bf9b5eb.js";const o={},s={class:"box"};function t(n,a){return _(),c("div",s)}const r=e(o,[["render",t],["__scopeId","data-v-e9e49326"]]);export{r as default};
